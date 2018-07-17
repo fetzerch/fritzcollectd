6b2580b156a451b64d99c0227b83f1227d6ab057
Christian Fetzer <fetzer.ch@gmail.com>
2018-07-17 08:02:17 +0200

    tox: Fix warnings reported by PyLint 2.0.0
    Disable these warnings as we need to remain compatible with Python 2.7.
    
    fritzcollectd.py:38: [R0205(useless-object-inheritance), FritzCollectd]
        Class 'FritzCollectd' inherits from object, can be safely removed
        from bases in python3
    test_fritzcollectd.py:35: [R0205(useless-object-inheritance),
        CollectdMock] Class 'CollectdMock' inherits from object, can be
        safely removed from bases in python3
    test_fritzcollectd.py:92: [R0205(useless-object-inheritance),
        CollectdConfig] Class 'CollectdConfig' inherits from object, can be
        safely removed from bases in python3
    test_fritzcollectd.py:111: [R0205(useless-object-inheritance),
        CollectdValues] Class 'CollectdValues' inherits from object, can be
        safely removed from bases in python3
    test_fritzcollectd.py:133: [R0205(useless-object-inheritance),
        FritzConnectionMock] Class 'FritzConnectionMock' inherits from
        object, can be safely removed from bases in python3

--- fritzcollectd.py
+++ fritzcollectd.py
@@ -19,6 +19,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# pylint: disable=useless-object-inheritance
+
 """ fritzcollectd - FRITZ!Box collectd plugin """
 
 from collections import namedtuple, OrderedDict
--- test_fritzcollectd.py
+++ test_fritzcollectd.py
@@ -19,6 +19,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# pylint: disable=useless-object-inheritance
+
 """ Tests for fritzcollectd """
 
 from __future__ import print_function
