95b1953fc520ecb551d05a63dda2dbfb78c11053
Christian Fetzer <fetzer.ch@gmail.com>
2018-10-20 17:21:11 +0200

    project: Update year in copyright headers to 2014-2018

--- tests/test_fritzcollectd.py
+++ tests/test_fritzcollectd.py
@@ -1,5 +1,5 @@
 # fritzcollectd - FRITZ!Box collectd plugin
-# Copyright (c) 2014-2017 Christian Fetzer
+# Copyright (c) 2014-2018 Christian Fetzer
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
